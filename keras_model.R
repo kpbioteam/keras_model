5a945ff7e27e95738990e5a4a9056fa55b26b056
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2018-03-16 15:52:25 +0000

    first commit

---
+++ keras_model.R
@@ -0,0 +1,33 @@
+require('keras')
+args <- commandArgs(trailingOnly = TRUE)
+
+input = args[1]
+output = args[2]
+
+mnist <- input
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+# reshape
+x_train <- array_reshape(x_train, c(nrow(x_train), 784))
+x_test <- array_reshape(x_test, c(nrow(x_test), 784))
+# rescale
+x_train <- x_train / 255
+x_test <- x_test / 255
+
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
+
+model <- keras_model_sequential() 
+model %>% 
+  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = 'softmax')
+
+summary <- summary(model)
+
+write.table(summary, output)
